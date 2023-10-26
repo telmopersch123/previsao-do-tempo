f2e591cca0d083641291ea269f0de0eaced0f3e8
telmopersch123 <benjamininmin@gmail.com>
2023-10-26 18:45:01 -0400

    29/10/2023

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -2,10 +2,109 @@ import React, { useState, useEffect, useRef } from "react";
 import moment from "moment";
 import { convertorFahrenheit } from "../Conv";
 import "./index.css";
-
+import ReactDOM from "react-dom";
 //Mapas//
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+const scaleDetailsMap = {
+  temp: (
+    <div className="scale-details">
+      <div className="scale-label">Temperatura °C</div>
+      <div className="organized-bar">
+        <div className="numbers-bar">
+          <p>-40</p>
+          <p>-20</p>
+          <p>0</p>
+          <p>20</p>
+          <p>40</p>
+        </div>
+        <div className="scale-bar">
+          <div className="scale-gradient"></div>
+        </div>
+      </div>
+    </div>
+  ),
+  pressure: (
+    <div className="scale-details">
+      <div className="scale-label">Pressão, hPa</div>
+      <div className="organized-bar">
+        <div className="numbers-bar pressao-numbers">
+          <p>950</p>
+          <p>980</p>
+          <p>1010</p>
+          <p>1040</p>
+          <p>1070</p>
+        </div>
+        <div className="scale-bar">
+          <div className="scale-gradient-pressure"></div>
+        </div>
+      </div>
+    </div>
+  ),
+  wind: (
+    <div className="scale-details">
+      <div className="scale-label">Velocidade do vento, m/s</div>
+      <div className="organized-bar">
+        <div className="numbers-bar wind-speed-numbers">
+          <p>0</p>
+          <p>2</p>
+          <p>3</p>
+          <p>6</p>
+          <p>12</p>
+          <p>25</p>
+          <p>50</p>
+          <p>100</p>
+        </div>
+        <div className="scale-bar">
+          <div className="scale-gradient-wind"></div>
+        </div>
+      </div>
+    </div>
+  ),
+  clouds: (
+    <div className="scale-details">
+      <div className="scale-label">Nuvens, %</div>
+      <div className="organized-bar">
+        <div className="numbers-bar">
+          <p>0</p>
+          <p>25</p>
+          <p>50</p>
+          <p>75</p>
+          <p>100</p>
+        </div>
+        <div className="scale-bar">
+          <div className="scale-gradient-clouds"></div>
+        </div>
+      </div>
+    </div>
+  ),
+  precipitation: (
+    <div className="scale-details">
+      <div className="scale-label">Precipitação, mm/h</div>
+      <div className="organized-bar">
+        <div className="numbers-bar precipitation-numbers">
+          <p>0</p>
+          <p>0.5</p>
+          <p>1</p>
+          <p>2</p>
+          <p>4</p>
+          <p>6</p>
+          <p>7</p>
+          <p>10</p>
+          <p>12</p>
+          <p>14</p>
+          <p>16</p>
+          <p>24</p>
+          <p>32</p>
+          <p>60</p>
+        </div>
+        <div className="scale-bar precipitation-bars">
+          <div className="scale-gradient-precipitation"></div>
+        </div>
+      </div>
+    </div>
+  ),
+};
 
 const Timeline = ({
   timeUpdate1,
@@ -17,6 +116,11 @@ const Timeline = ({
   lat,
   lon,
 }) => {
+  const ScaleDetails = ({ mapLayer }) => {
+    // Use a variável scaleDetailsMap definida no escopo mais amplo
+    return <div className="info legend">{scaleDetailsMap[mapLayer]}</div>;
+  };
+
   const updateFormattedTime = (time) =>
     moment(time, "DD-MM-YYYY HH:mm:ss").format("HH:mm");
 
@@ -44,10 +148,21 @@ const Timeline = ({
     }
   }, [Celsius, temp]);
 
+  const [mapLayer, setMapLayer] = useState("temp"); // Inicialmente, exibe a camada de temperatura
   const [temperatureDisplay, setTemperatureDisplay] = useState(temp);
   const mapRef = useRef(null);
   const mapInstance = useRef(null);
+  //Inicialmente, exibe os detalhes de temperatura
+  const [scaleDetailsContent, setScaleDetailsContent] = useState(
+    scaleDetailsMap["temp"],
+  );
+  const [currentSituation, setCurrentSituation] = useState("temp"); // Comece com "temp" como padrão
 
+  const changeMapLayer = (newLayer) => {
+    setMapLayer(newLayer);
+    setScaleDetailsContent(scaleDetailsMap[newLayer]);
+    setCurrentSituation(newLayer);
+  };
   useEffect(() => {
     // Verifica se o mapa já foi inicializado
     if (mapRef.current) {
@@ -64,8 +179,8 @@ const Timeline = ({
         maxBounds: maxBounds, // Defina os limites máximos
         maxBoundsViscosity: 1.0, // Isso ajuda a manter o mapa dentro dos limites
       }).setView([lat, lon], 10); // Defina o zoom inicial
-      const temperatureLayer = L.tileLayer(
-        "https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=4d8fb5b93d4af21d66a2948710284366",
+      const weatherLayer = L.tileLayer(
+        `https://tile.openweathermap.org/map/${mapLayer}_new/{z}/{x}/{y}.png?appid=4d8fb5b93d4af21d66a2948710284366`,
         {
           maxZoom: 10,
           minZoom: 3,
@@ -85,42 +200,60 @@ const Timeline = ({
       );
 
       openStreetMapLayer.addTo(map);
-      temperatureLayer.addTo(map);
+      weatherLayer.addTo(map);
       const temperatureControl = L.control({ position: "bottomleft" });
       temperatureControl.onAdd = function (map) {
-        const div = L.DomUtil.create("div", "info legend");
-        div.innerHTML = `
-        <div class="scale-details">
-          <div class="scale-label">Temperatura °C </div>
-          <div class="organized-bar">
-            <div class="numbers-bar">
-              <p>-40</p>
-              <p>-20</p>
-              <p>0</p>
-              <p>20</p>
-              <p>40</p>
-            </div>
-            <div class="scale-bar">
-              <div class="scale-gradient"></div>
-            </div>
-          </div>
-        </div>
-      `;
+        const scaleDetails = <ScaleDetails mapLayer={mapLayer} />;
+        const div = L.DomUtil.create("div", "custom-legend");
+        ReactDOM.createRoot(div).render(scaleDetails);
         return div;
       };
       openStreetMapLayer.addTo(map);
       temperatureControl.addTo(map);
       mapInstance.current = map;
     }
-  }, [lat, lon]);
+  }, [lat, lon, mapLayer, scaleDetailsContent]);
 
   return (
     <div className="itens_prim">
+      <div className="map-layer-buttons">
+        <button
+          onClick={() => changeMapLayer("temp")}
+          className={mapLayer === "temp" ? "active" : ""}
+        >
+          Temperature
+        </button>
+        <button
+          onClick={() => changeMapLayer("pressure")}
+          className={mapLayer === "pressure" ? "active" : ""}
+        >
+          Pressure
+        </button>
+        <button
+          onClick={() => changeMapLayer("wind")}
+          className={mapLayer === "wind" ? "active" : ""}
+        >
+          Wind Speed
+        </button>
+        <button
+          onClick={() => changeMapLayer("clouds")}
+          className={mapLayer === "clouds" ? "active" : ""}
+        >
+          Clouds
+        </button>
+        <button
+          onClick={() => changeMapLayer("precipitation")}
+          className={mapLayer === "precipitation" ? "active" : ""}
+        >
+          Precipitation
+        </button>
+      </div>
       <div
         ref={mapRef}
         id="map"
-        style={{ width: "800px", height: "600px" }}
+        className={`map-container ${currentSituation}`}
       ></div>
+
       <p className={temp !== undefined ? "" : "dados_ind"}>
         {temp !== undefined ? (
           `Temperatura de ${temperatureDisplay.toFixed(0)} ${
